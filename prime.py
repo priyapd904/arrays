a2013818caa8c128d8a45fb09446c320cd5b27ac
priyapd904 <53601382+priyapd904@users.noreply.github.com>
2019-08-07 14:47:58 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,1 +1,9 @@
+num = 67
+if num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           break
+   else:
+       print(num,"is a prime number")
 
