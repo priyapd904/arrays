3159d0d7fc0942e0162e69e91d68f9ab92e3c985
priyapd904 <53601382+priyapd904@users.noreply.github.com>
2019-08-07 15:24:30 +0530

    Update patterns num.py

--- patterns num.py
+++ patterns num.py
@@ -1,1 +1,15 @@
-
+num=1
+count=0
+decr=8
+for i in range(0, 5):
+    for k in range(0,decr):
+        print(end=" ")
+    for j in range(0,i):
+        count=count+1
+    num=count
+    temp=num
+    for j in range(0,i):
+        print(num,end=" ")
+        num=num-1
+    print()
+    num=temp
